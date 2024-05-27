6db95bb07fb075d4a3dea923dab908a9607d0b70
gouthami24 <gouthami.santhanam@gmail.com>
2024-05-27 10:56:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,86 @@
 import os
 import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from langchain.agents import create_openai_functions_agent, AgentExecutor
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
+from langchain.chains import create_retrieval_chain
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain.tools.retriever import create_retriever_tool
+from langchain_community.tools.tavily_search import TavilySearchResults
 
-from dotenv import load_dotenv, find_dotenv
-_= load_dotenv(find_dotenv())
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+tavily_api_key = st.sidebar.text_input('Tavily API Key', type='password')
 
-myvar = os.getenv('TAVILY_API_KEY')
-openapikey= "sk-proj-A7WaN8QzHzHfX1gxAWtQT3BlbkFJBBuT10WWn44MP9B6A3PM"
-st.write(myvar)
+
+# Create Retriever
+loader = WebBaseLoader("https://python.langchain.com/docs/expression_language/")
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=200,
+    chunk_overlap=20
+)
+splitDocs = splitter.split_documents(docs)
+
+embedding = OpenAIEmbeddings(api_key = openapikey)
+vectorStore = FAISS.from_documents(docs, embedding=embedding)
+retriever = vectorStore.as_retriever(search_kwargs={"k": 3})
+
+model = ChatOpenAI(
+    model='gpt-3.5-turbo-1106',
+    temperature=0.7,
+    api_key = openapikey
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a friendly assistant called Max."),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("human", "{input}"),
+    MessagesPlaceholder(variable_name="agent_scratchpad")
+])
+
+search = TavilySearchResults()
+
+retriever_tools = create_retriever_tool(
+    retriever,
+    "lcel_search",
+    "Use this tool when searching for information about Langchain Expression Language (LCEL)."
+)
+tools = [search, retriever_tools]
+
+agent = create_openai_functions_agent(
+    llm=model,
+    prompt=prompt,
+    tools=tools
+)
+
+agentExecutor = AgentExecutor(
+    agent=agent,
+    tools=tools
+)
+
+def process_chat(agentExecutor, user_input, chat_history):
+    response = agentExecutor.invoke({
+        "input": user_input,
+        "chat_history": chat_history
+    })
+    return response["output"]
+
+if __name__ == '__main__':
+    chat_history = []
+
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            break
+
+        response = process_chat(agentExecutor, user_input, chat_history)
+        chat_history.append(HumanMessage(content=user_input))
+        chat_history.append(AIMessage(content=response))
+
+        print("Assistant:", response)
