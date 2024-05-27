70b617c7dce7d67b40557f5a6f5f690ea67c8fac
gouthami24 <gouthami.santhanam@gmail.com>
2024-05-27 16:06:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,20 +78,29 @@ def process_chat(agentExecutor, user_input, chat_history):
     })
     #st.write("Response:",response["output"])
     return response["output"]
-
+   
+def submit():
+    st.session_state.my_text = st.session_state.widget
+    st.session_state.widget = ""
+   
 if __name__ == '__main__':
     chat_history = []
-
     while True:
-        user_input = st.text_input("You: ")
-        if user_input.lower() == 'exit':
-            break
+       if "my_text" not in st.session_state:
+          st.session_state.my_text = ""
+       st.text_input("Enter text here", key="widget", on_change=submit)
+       my_text = st.session_state.my_text
+       st.write(my_text)
 
-        response = process_chat(agentExecutor, user_input, chat_history)
-        chat_history.append(HumanMessage(content=user_input))
-        chat_history.append(AIMessage(content=response))
-        print(HumanMessage(content=user_input))
-        print(AIMessage(content=response))
-        #st.write(chat_history["content"])
-        st.write("Assistant:", response)
-        st.write("Test")
+    # while True:
+    #     user_input = st.text_input("You: ")
+    #     if user_input.lower() == 'exit':
+    #         break
+
+    #     response = process_chat(agentExecutor, user_input, chat_history)
+    #     chat_history.append(HumanMessage(content=user_input))
+    #     chat_history.append(AIMessage(content=response))
+    #     print(HumanMessage(content=user_input))
+    #     print(AIMessage(content=response))
+    #     #st.write(chat_history["content"])
+    #     st.write("Assistant:", response)
