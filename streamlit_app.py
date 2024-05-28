7b0ea20c3bf1c31407e4d2c94dabd5b1621fcee8
gouthami24 <gouthami.santhanam@gmail.com>
2024-05-28 12:29:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ elif openai_api_key.startswith('sk-') and tavily_api_key:
        MessagesPlaceholder(variable_name="agent_scratchpad")
    ])
 
-   search = TavilySearchResults(api_key=tavily_api_key)
+   search = TavilySearchResults(api_key = tavily_api_key)
   
    retriever_tools = create_retriever_tool(
        retriever,
@@ -85,7 +85,6 @@ elif openai_api_key.startswith('sk-') and tavily_api_key:
    # Accept user input
    if user_input := st.chat_input("What is up?"):
     # Add user message to chat history
-      #st.session_state.chat_history.append({"role": "user", "content": user_input})
       response = process_chat(agentExecutor, user_input, st.session_state.chat_history)
       st.session_state.chat_history.append(HumanMessage(content=user_input))
       st.session_state.chat_history.append(AIMessage(content=response))
