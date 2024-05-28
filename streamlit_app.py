e4eda054bfe5e03958cc879b7dfb02d4b69d4846
gouthami24 <gouthami.santhanam@gmail.com>
2024-05-27 17:08:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,12 +107,12 @@ if __name__ == '__main__':
     # Display assistant response in chat message container
        with st.chat_message("assistant"):
             stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
+               model=st.session_state["openai_model"],
+               messages=[
+                   {"role": m["role"], "content": m["content"]}
+                   for m in st.session_state.messages
+               ],
             stream=True,
-        )
-        response = st.write_stream(stream)
+            )
+      response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
